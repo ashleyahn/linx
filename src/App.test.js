4c63b32f6182e3a82b42780b9c313e522bde7d7b
Ashley Ahn <ashleyjsahn@gmail.com>
2020-06-09 21:59:24 -0700

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import BackButton from './BackButton';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('back button renders', () => {
+  let mockfn = jest.fn();
+  const pathState = {path: "/home/cs394", setPath: mockfn}
+  const { getByTestId } = render(<BackButton state = {pathState}/> );
+  const backComp = getByTestId("backButton");
+  fireEvent.click(backComp);
+  expect(backComp).toBeInTheDocument();
+}); 
